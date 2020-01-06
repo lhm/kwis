6d20d8e15ac4288c834780fb9bb79bf71bd76977
Lars Henrik Mai <lars.mai@kontinui.de>
2020-01-06 14:13:44 +0100

    Use Postgres as data store

--- scripts/build-prtr.sh
+++ scripts/build-prtr.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 ogr2ogr -f GPKG prtr_emissions.gpkg prtr_emissions.geojson
+
+# csv to postgis
+# ogr2ogr -f PGDump prtr-emissions.sql data/prtr-emissions.csv -oo X_POSSIBLE_NAMES=geo_long* -oo Y_POSSIBLE_NAMES=geo_lat* -a_srs 'EPSG:4326'
