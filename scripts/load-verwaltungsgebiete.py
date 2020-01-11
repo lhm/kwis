ace36079e64430d2020e4f3f0b50aecdde42d022
Lars Henrik Mai <lars.mai@kontinui.de>
2020-01-11 12:12:28 +0100

    Load district areas from datapackage

--- scripts/load-verwaltungsgebiete.py
+++ scripts/load-verwaltungsgebiete.py
@@ -1,13 +1,17 @@
+from datapackage import Package
 import geopandas
 from sqlalchemy import create_engine
 from pathlib import Path
 
 root = Path(__file__).parents[1]
 
-df = geopandas.read_file(root / 'data/vg250-landkreise.geojson')
+package = Package('https://raw.githubusercontent.com/lhm/verwaltungsgebiete/master/datapackage.json')
+resource = package.get_resource('vg250-districts')
+
+df = geopandas.read_file(resource.source)
 # Regions that have sea territories have two entries. For now,
 # select only land area in order to get unique records.
 df = df[df.GF == 4]
 
 engine = create_engine('postgresql+psycopg2://docker:docker@localhost:5432/gis')
-df.to_postgis(con=engine, name='vg250_krs', if_exists='replace')
+df.to_postgis(con=engine, name='vg250_districts', if_exists='replace')
